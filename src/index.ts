a79402e39079f928bb5f42c6357326d7523c34e7
Natalia Gherardi <natirgh@gmail.com>
2022-08-12 22:30:39 +0200

    added index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import "./environmentDotenv";
+import express from "express";
+import Debug from "debug";
 
+const debug = Debug("my-robots:files:index");
+debug("hola");
+
+const port = process.env.PORT ?? 4000;
+
+const app = express();
+
+app.listen(port, () => {
+  debug("si");
+});
