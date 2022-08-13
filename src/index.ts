3a0db2d49a7cab51b661e6da6c5b2ec138503d06
Andrea Martinez <andrea.maort@gmail.com>
2022-08-13 12:32:04 +0200

    added notFoundError and its test

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import "./environmentDotenv";
 import express from "express";
 import Debug from "debug";
+import chalk from "chalk";
+import morgan from "morgan";
+import notFoundError from "./middlewares/errors";
 
 const debug = Debug("my-robots:files:index");
-debug("hola");
-
 const port = process.env.PORT ?? 4000;
-
 const app = express();
 
+app.use(morgan("dev"));
+
+app.use(notFoundError);
+
 app.listen(port, () => {
-  debug("si");
+  debug(chalk.yellow(`It is listening in ${port}`));
 });
+
+export default app;
