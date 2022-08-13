e5ba6e952c823bad4f8cbe321dbcf0ce758d1abb
Andrea Martinez <andrea.maort@gmail.com>
2022-08-13 13:13:01 +0200

    added get controller

---
+++ src/controllers/controllers.ts
@@ -0,0 +1,13 @@
+import chalk from "chalk";
+import Debug from "debug";
+import { Request, Response } from "express";
+import robots from "../data-provisional/data";
+
+const debug = Debug("my-robots:src:controllers:controllers.ts");
+
+const getRobots = (req: Request, res: Response) => {
+  res.json({ robots });
+  debug(chalk.green("We have printed all robots! :)"));
+};
+
+export default getRobots;
