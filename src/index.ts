619707202c1a5219a8b6a6385afffe848c39c854
Andrea Martinez <andrea.maort@gmail.com>
2022-08-16 21:40:59 +0200

    added users controller

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import cors from "cors";
 import express from "express";
 import "./environmentDotenv";
 import morgan from "morgan";
-import notFoundError, { generalError } from "./server/middlewares/errors";
+import { notFoundError, generalError } from "./server/middlewares/errors";
 import routersRobots from "./server/routers/routersRobots";
 import startServer, { app } from "./server/server";
 import connectDB from "./database";
+import usersRobots from "./server/routers/userRouter";
+import usersRouter from "./server/routers/userRouter";
 
 const port = process.env.PORT ?? 4000;
 const mongoURL = process.env.MONGO_URL as string;
@@ -33,6 +35,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/robots", routersRobots);
+app.use("/users", usersRouter);
 
 app.use(notFoundError);
 
